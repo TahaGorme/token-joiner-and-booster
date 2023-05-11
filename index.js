3c5aca4f5bc725626228ed65dbba5720462d7495
Taha Gorme <63650975+TahaGorme@users.noreply.github.com>
2023-05-11 08:54:00 -0700

    an important update
    fixed proxy

--- index.js
+++ index.js
@@ -10,47 +10,47 @@ process.on("uncaughtException", (err, origin) => {
   // Application specific logging, throwing an error, or other logic here
 });
 const gradient = require('gradient-string');
-
+var retry = []
 const config = require("./config.json");
-const { Client } = require("discord.js-selfbot-v13");
+const { Client } = require("discord.js-selfbot-v13-proxy");
 var totalJoined = 0;
 var failed = 0;
 const HttpsProxyAgent = require('https-proxy-agent');
 const fs = require("fs");
 //read from tokens.txt
 
+const proxies = fs.readFileSync("proxies.txt").toString().split("\n");
 
 
 console.log(gradient.rainbow("Token Joiner by T4H4"));
 
 async function readTokens() {
   const tokens = fs.readFileSync("tokens.txt").toString().split("\n");
-
+  var j = 1;
   for (i in tokens) {
-    await new Promise((resolve) => setTimeout(resolve, i * config.joinDelay));
+    // await new Promise((resolve) => setTimeout(resolve, i * 100));
     doEverything(
       tokens[i]?.trim()?.replace("\r", "")?.replace("\n", ""),
-      tokens
+      tokens,
+      j
     );
+    j++;
   }
 }
 readTokens();
-const proxies = fs.readFileSync("proxies.txt").toString().split("\n");
 
-async function doEverything(token, tokens) {
-  // console.log(token);
+async function doEverything(token, tokens, i) {
   const randomProxy = proxies[Math.floor(Math.random() * proxies.length)]?.replace("\r", "")?.replace("\n", "");
   var client;
   if (config.useProxies) {
 
 
-    var agent = HttpsProxyAgent(randomProxy);
     client = config.captcha_api_key
       ? new Client({
         captchaService: config.captcha_service.toLowerCase(),
         captchaKey: config.captcha_api_key,
         checkUpdate: false,
-        http: { agent: agent },
+        proxy: randomProxy,
         restRequestTimeout: 60 * 1000,
         interactionTimeout: 60 * 1000,
         restWsBridgeTimeout: 5 * 1000
@@ -71,55 +71,78 @@ async function doEverything(token, tokens) {
   client.on("ready", async () => {
     console.log(chalk.green("Logged in as ") + gradient.cristal(client.user.tag));
 
+    setTimeout(async () => {
+      await client
+        .fetchInvite(config.inviteCode)
+        .then(async (invite) => {
 
-    await client
-      .fetchInvite(config.inviteCode)
-      .then(async (invite) => {
-
-        await invite
-          .acceptInvite(true)
-          .then(async () => {
-            console.log(chalk.greenBright(`Joined as ${gradient.passion(client.user.tag)}`));
-            totalJoined++;
-            process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
-
-            if (client.token === tokens[tokens.length - 1]) {
-              console.log(`${chalk.magentaBright("[INFO]")} Joined ${gradient.passion(totalJoined)} servers and failed to join ${gradient.passion(failed)} servers}`)
-
-              process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
-            }
-
-            if (config.boost.enabled) {
-              setTimeout(async () => {
-                const allBoosts = await client.billing.fetchGuildBoosts();
-                const firstBoost = allBoosts.first();
-                await firstBoost.subscribe(config.boost.serverId);
-                console.log(`${chalk.greenBright("[SUCCESS]")} Boosted Server as ${gradient.cristal(client.user.tag)}}`);
-              }, config.boost.delay);
-            }
-          })
-          .catch((err) => {
-            console.log(`${chalk.redBright("[ERROR]")} Failed to join as ${gradient.fruit(client.user.tag)}`);
-            failed++;
-            process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
-
-
-            console.error(chalk.redBright(err));
-
-            if (client.token === tokens[tokens.length - 1]) {
-              console.log(`${chalk.magentaBright("[INFO]")} Joined ${gradient.passion(totalJoined)} servers and failed to join ${gradient.passion(failed)} servers}`)
-
+          await invite
+            .acceptInvite(true)
+            .then(async () => {
+              console.log(chalk.greenBright(`Joined as ${gradient.passion(client.user.tag)}`));
+              totalJoined++;
               process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
 
-            }
-          });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+              // var data = fs.readFileSync("tokens.txt", "utf-8");
+              // fs.writeFileSync("tokens.txt", data.replace(token+"\n", ""));
+
+              if (client.token === tokens[tokens.length - 1]) {
+                console.log(`${chalk.magentaBright("[INFO]")} Joined ${gradient.passion(totalJoined)} servers and failed to join ${gradient.passion(failed)} servers}`)
+
+                process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
+              }
+
+              if (config.boost.enabled) {
+                setTimeout(async () => {
+                  const allBoosts = await client.billing.fetchGuildBoosts();
+                  const firstBoost = allBoosts.first();
+                  await firstBoost.subscribe(config.boost.serverId);
+                  console.log(`${chalk.greenBright("[SUCCESS]")} Boosted Server as ${gradient.cristal(client.user.tag)}}`);
+                  client.destroy();
+                }, config.boost.delay);
+              } else {
+                client.destroy();
+              }
+            })
+            .catch((err) => {
+              console.log(`${chalk.redBright("[ERROR]")} Failed to join as ${gradient.fruit(client.user.tag)}`);
+              failed++;
+              process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
+
+
+              console.error(chalk.redBright(err));
+
+              if (client.token === tokens[tokens.length - 1]) {
+                console.log(`${chalk.magentaBright("[INFO]")} Joined ${gradient.passion(totalJoined)} servers and failed to join ${gradient.passion(failed)} servers}`)
+
+                process.title = `Joined: ${totalJoined} | Failed: ${failed}`;
+
+              }
+
+              if (err?.toString()?.includes("DiscordAPIError: Unknown Message")) {
+                if (!retry.includes(client.user.id)) {
+                  setTimeout(async () => {
+                    retry.push(client.user.id)
+                    doEverything(token, tokens, i);
+                  }, 10000)
+                }
+                else client.destroy();
+
+              } else {
+                client.destroy();
+              }
+            });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }, config.joinDelay * i);
   });
   client.login(token).catch(() => {
     console.log(`${chalk.redBright("[ERROR]")} Invalid token ${gradient.instagram(token)}}`);
+    var data = fs.readFileSync("tokens.txt", "utf-8");
+    fs.writeFileSync("tokens.txt", data.replace(token + "\n", ""));
+
     if (client.token === tokens[tokens.length - 1]) {
       console.log(`${chalk.magentaBright("[INFO]")} Joined ${gradient.passion(totalJoined)} servers and failed to join ${gradient.passion(failed)} servers}`)
 
